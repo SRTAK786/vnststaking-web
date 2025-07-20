ca5471f64ec95b67f24fbf9a76e3901a2c840e2c
SRTAK786 <sonysangramsillwer786@gmail.com>
2025-07-20 06:51:03 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -164,4 +164,4 @@ window.addEventListener('DOMContentLoaded', async () => {
   }
 });
 
-export { web3, accounts, isConnected, vnstTokenContract, stakingContract, connectWallet };
+export { web3, accounts, isConnected, vnstTokenContract, stakingContract, connectWallet, initWeb3 };
